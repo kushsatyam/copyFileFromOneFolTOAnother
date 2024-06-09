8b601e3362ad6b1d6bbf129a2938d420434245db
satyam kushwaha <satyam8871691032@gmail.com>
2024-06-09 15:47:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+
+const srcPathName = path.join(__dirname, 'temp');
+const desPathName = path.join(__dirname, 'upload');
+// console.log(pathName);
+
+fs.readdir(srcPathName, (err, data) => {
+    if (err) return;
+
+    // console.log(data);
+    for (let item of data) {
+        copFiles(item);
+    }
+
+});
+
+function toEmptyTheTemp(fileName) {
+    fs.rm(srcPathName+'/'+fileName,(err)=>{
+        console.log(err);
+    })
+}
+
+
+function copFiles(fileName) {
+    fs.copyFile(srcPathName + '/' + fileName, desPathName + '/' + fileName, (err) => {
+        if (err) {
+            console.log(err)
+            return;
+        }
+        toEmptyTheTemp(fileName);
+        // console.log('success');
+    });
+}
